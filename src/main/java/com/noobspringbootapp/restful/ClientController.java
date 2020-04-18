7edcb3b15af46f149b81bf2ced302086598ea04c
Tan Chin Leong <cltan8@gmail.com>
2020-04-18 12:04:28 +0800

    Handle cors.

--- src/main/java/com/noobspringbootapp/restful/ClientController.java
+++ src/main/java/com/noobspringbootapp/restful/ClientController.java
@@ -3,6 +3,7 @@ package com.noobspringbootapp.restful;
 import com.noobspringbootapp.service.Client;
 import com.noobspringbootapp.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,6 +15,7 @@ public class ClientController {
     @Autowired
     ClientService client;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @RequestMapping("/clients")
     public List<Client> getClients() {
         return client.getAll();
