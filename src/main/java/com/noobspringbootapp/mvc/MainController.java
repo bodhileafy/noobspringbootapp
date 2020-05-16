ea640771581da966a34001d0ccbc6229185ff5ee
Tan Chin Leong <cltan8@gmail.com>
2020-05-16 09:07:14 +0800

    Demo Application Properties.

--- src/main/java/com/noobspringbootapp/mvc/MainController.java
+++ src/main/java/com/noobspringbootapp/mvc/MainController.java
@@ -1,14 +1,18 @@
 package com.noobspringbootapp.mvc;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
+    @Value("${tan.chin.leong}")
+    String externalname;
+
     @RequestMapping("/")
     public String getIndex() {
-        return "Greetings from Main";
+        return "Greetings from Main-"+externalname;
     }
 
 }
